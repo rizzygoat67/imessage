c35577d193f4e3a4b64f818eeb7d9fe6a16bcbdd
Your Name <you@example.com>
2026-06-13 14:23:12 +0500

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is up on port:", PORT);
+});
