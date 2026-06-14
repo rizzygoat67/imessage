648ab2fbcd34f49da54eba0f1ae1c80bd913b4f3
Your Name <you@example.com>
2026-06-14 12:47:10 +0500

    Add MongoDB connection and define user and message models

---
+++ backend/src/lib/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+export async function connectDB() {
+  try {
+    const mongoUri = process.env.MONGO_URI;
+
+    if (!mongoUri) {
+      throw new Error("MONGO_URI is required");
+    }
+
+    const conn = await mongoose.connect(mongoUri);
+
+    await mongoose.connect(mongoUri);
+    console.log("MongoDB connected", conn.connection.host);
+  } catch (error) {
+    console.log("MongoDB connnection error:", error.message);
+    process.exit(1);
+    // 1 means failed, 0 means success
+  }
+}
