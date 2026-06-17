43fef80c21dd068bdf44f47c97aa58a8750028ed
rizzygoat67 <yoyo.coolestguy@gmail.com>
2026-06-17 13:49:20 +0500

    Refactor server setup and fix webhook route in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,8 @@
-// Add this at line 1 of src/index.js
-import dns from "node:dns/promises";
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
-
 import express from "express";
 import cors from "cors";
 
 import "dotenv/config";
+
 import fs from "fs";
 import path from "path";
 
@@ -16,15 +13,17 @@ import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
 
 import clerkWebhook from "./webhooks/clerk.webhook.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
 
 // it's important that you don't parse the webhook event data, it should be in the raw format
-app.use("api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
+app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
 
 app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
@@ -34,9 +33,11 @@ app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
 });
 
-// if the public directory exists, serve the stastic files
-// this is for the prodcution build
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
+// if the public directory exists, serve the static files
+// this is for the production build
 if (fs.existsSync(publicDir)) {
   app.use(express.static(publicDir));
 
@@ -45,7 +46,7 @@ if (fs.existsSync(publicDir)) {
   });
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectDB();
   console.log("Server is up and running on PORT:", PORT);
 
