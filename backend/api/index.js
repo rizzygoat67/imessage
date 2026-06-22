1aa65303c673c753840de47efc4f723839e4b524
rizzygoat67 <yoyo.coolestguy@gmail.com>
2026-06-22 11:13:04 +0500

    test vercel api

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,36 +1,3 @@
-import express from "express";
-import serverless from "serverless-http";
-import cors from "cors";
-
-import { clerkMiddleware } from "@clerk/express";
-
-import clerkWebhook from "../src/webhooks/clerk.webhook.js";
-import authRoutes from "../src/routes/auth.route.js";
-import messageRoutes from "../src/routes/message.route.js";
-
-const app = express();
-
-const FRONTEND_URL = process.env.FRONTEND_URL;
-
-// Clerk webhook first
-app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
-
-app.use(express.json());
-
-app.use(
-  cors({
-    origin: FRONTEND_URL,
-    credentials: true,
-  }),
-);
-
-app.use(clerkMiddleware());
-
-app.get("/", (req, res) => {
-  res.json({ ok: true });
-});
-
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-
-export default serverless(app);
+export default function handler(req, res) {
+  res.status(200).json({ working: true });
+}
